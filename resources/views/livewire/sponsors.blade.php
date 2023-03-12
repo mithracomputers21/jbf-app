5cd9939c51cd18e7409a0d64418507de396e01ad
mithracomputers21 <mithracomputers21@gmail.com>
2023-03-12 10:03:55 +0530

    user model update

--- resources/views/livewire/sponsors.blade.php
+++ resources/views/livewire/sponsors.blade.php
@@ -1,5 +1,6 @@
 <div class="w-screen flex item-center justify-center">
     <div class="max-w-lg mx-auto p-10 border border-gray-200 rounded-lg">
+        <h1 class="text-blue">புதிய படிப்பகம் கட்ட அல்லது இயங்கும் படிப்பகத்தை இணைக்க விண்ணப்பம்</h1>
     <form wire:submit.prevent="submit">
         {{ $this->form }}
     
